f40175832a5326d7298075d5f5f7fc09f407c968
andreisuslov <truvord@gmail.com>
2024-04-18 13:47:48 -0400

    Rename and update PerformQuerySearch test

---
+++ src/test/groovy/test/PerformQuerySearchTest.groovy
@@ -0,0 +1,34 @@
+package test
+
+
+
+import io.qameta.allure.Description
+import org.testng.annotations.Test
+import pageobjects.GooglePage
+import pageobjects.SearchResults
+import parcers.DP
+import parcers.TestParameters
+
+
+/**
+ * This test will perform a search query to verify search results
+ * @query -> text we would like to search via google search bar
+ */
+class PerformQuerySearchTest {
+
+    def googlePage = new GooglePage()
+    def searchResults = new SearchResults()
+
+    @Test(dataProvider = "DP", dataProviderClass = DP)
+    @TestParameters(['query', 'resultTitle'])
+    @Description("Search query on google website")
+    void performSearch(String query, String resultTitle) {
+        GooglePage google = new GooglePage()
+        SearchResults results = new SearchResults()
+        println("Searching for: $query")
+        google.open()
+        google.search(query)
+        results.shouldHaveResult(resultTitle);
+    }
+}
+
